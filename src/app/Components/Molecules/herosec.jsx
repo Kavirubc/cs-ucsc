f20266cb39e6b3033a37d0bad73ed198095c01d7
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2023-12-13 07:46:43 +0530

    Update next.config.js and herosec.jsx, add Bloglist component

--- src/app/Components/Molecules/herosec.jsx
+++ src/app/Components/Molecules/herosec.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const HeroSec = () => {
     return (
         <div className="bg-black">
-            <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-8">
+            <div className="container mx-auto py-32 px-4 sm:px-6 lg:px-8">
                 <div className="text-center">
                     <h1 className="text-4xl font-bold text-white">Welcome Blog</h1>
                     <p className="mt-4 text-lg text-white">Blog of IEEE UCSC CS Chapter</p>
