4af6b293f485187f0c2b56bdb444b34304ab3a46
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2023-12-13 17:31:05 +0530

    Add @fontsource/poppins package and update blogtile component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,24 @@
 import HeroSec from './Components/Molecules/herosec';
 import Bloglist from './Components/bloglist';
+import Link from 'next/link';
 
 
 
 
 export default function Main() {
+
   return (
+
     <>
+  
 
       <div style={{ minHeight: 'calc(100vh - 100px)' }}>
         <HeroSec />
-        <div className='container mx-auto border px-10'>
-          This is where the blogs go
+        
+        <div className='container mx-auto mt-8 px-10'>
           <Bloglist />
         </div>
       </div>
-
-
     </>
   );
 }
